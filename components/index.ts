5a2794a1fe7463aa2705b7205454b1c43f344392
Supriyo Saha <mm.saha1982@gmail.com>
2023-06-17 18:09:37 +0530

    combobox created in search

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,11 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+
+
 
 
 export{
@@ -9,4 +14,7 @@ export{
     CustomButton,
     Navbar,
     Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer
 }
